32cb09c9c86d697cb54117d1b25e4f10712fc206
Renan Ferreira <renansantiago2013@hotmail.com>
2021-11-05 10:37:13 -0300

    Adiciona estilos globais

---
+++ src/styles/global.js
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: #DCDCDC;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  input, textarea, select, button, h1, h2, h3, h4, h5, h6 {
+    font-family: 'Zilla Slab', serif;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+`;
